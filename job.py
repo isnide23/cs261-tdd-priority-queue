8de6b87082abaf0dd836899b52d095fbbfd28833
Ian Snyder <isnide23@gmail.com>
2020-11-18 18:28:19 -0800

    Finished Job

--- job.py
+++ job.py
@@ -4,4 +4,39 @@
 
 class Job:
 
-    pass
+    def __init__(self, priority = None, message = None):
+        self.priority = priority
+        self.message = message
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+        
+    def __gt__(self, other):
+        return self.priority > other.priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def __ge__(self, other):
+        return self.priority >= other.priority
+
+    def __le__(self, other):
+        return self.priority <= other.priority
+
+    def __repr__(self):
+        return 'Job %s: %s' % (self.priority, self.message)
+
+    
+
+
+    
+
+    
+
+    
+
+
+
+    
+
+    
